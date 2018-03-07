8b46da54e7266eee8f4ac0d9f7470cb1e78a6930
qijuju <Oranq_qj@163.com>
2018-03-07 21:42:34 +0800

    3.7 合并一个filter.vue的我的代码

--- src/components/Home/Search.vue
+++ src/components/Home/Search.vue
@@ -1,34 +1,102 @@
 <template>
   <div class="search">
-    <van-nav-bar
-      v-bind:title="'搜索页面'" fixed
-      left-text="返回"
-      @click-left="onClickLeft"
-    >
+    <!-- 施工日计划待我审批搜索-表头-开始 -->
+    <van-nav-bar title="搜索" left-text="返回" @click-left="onClickLeft" >
     </van-nav-bar>
-
-
+    <!-- 施工日计划待我审批搜索-表头-结束 -->
+    <!-- 施工日计划待我审批搜索-输入项目名称-开始 -->
+    <form action="/">
+      <!--<van-search-->
+      <!--v-model="value"-->
+      <!--placeholder="请输入项目名称"-->
+      <!--show-action-->
+      <!--@search="onSearch"-->
+      <!--@cancel="onCancel"-->
+      <!--/>-->
+      <van-search
+        v-model="requestName"
+        placeholder="请输入流程标题"
+        show-action
+        @search="onSearch"
+      >
+        <div slot="action" @click="onSearch" style="padding: 0 10px;">搜索</div>
+      </van-search>
+    </form>
+    <!-- 施工日计划待我审批搜索-输入项目名称-结束 -->
   </div>
 </template>
 
 <script>
-  export default {
-    name: "search",
-    data(){
-      return{
 
+  import { Toast } from 'vant';
+  import Vue from 'vue';
+  import axios from 'axios';
+
+  export default {
+    name:'HomeSearch',
+    data() {
+      return {
+        requestName:'',
+        toDoWorkIsShow:true,
+        doneWorkIsShow:false,
+        iStartedIsShow:false,
+        requestName:''
       }
     },
-    components:{
+    mounted: function() {
+      this.disnone();
+      this.disblock();
     },
-    methods:{
+    methods: {
+      disnone(){
+        $("input").focus(function(){
+          $(".van-tabbar--fixed").hide();
+        });
+      },
+      disblock(){
+        $("input").blur(function(){
+          setInterval(function () {
+            $(".van-tabbar--fixed").show();
+          },1000);
+
+        });
+      },
+      // 表头返回按钮事件
       onClickLeft() {
-        this.$router.push({path:'/Home'});
+        this.$router.push({path: '/Home'});
+      },
+      // 表头关闭按钮事件
+      onClickRight() {
+        this.$router.push({path: '/Home'});
+      },
+      //  点击搜索按钮跳转事件
+      onSearch(){
+        //向待我审批页面展示-传递流程名称 变量名为v-model
+        var query={
+          requestName:this.requestName,
+        }
+        this.$router.push({path: '/Home',query:query});
+      },
+      // 点击取消按钮跳转事件
+      onCancel(){
+        this.$router.push({path: '/Home'});
       }
+
     }
-  }
+  };
+
 </script>
 
 <style scoped>
+
+  /* 设置头部 style start */
+  .van-nav-bar{
+    background: #2196F3;
+    color: #fff;
+  }
+  .van-nav-bar .van-icon{
+    color: #fff;
+  }
+  /* 设置头部 style end */
 
 </style>
